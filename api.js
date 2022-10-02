000e5581fa502011628dd686c582f6da47388ca5
andruxakonst <andruxakonst@yandex.ru>
2022-10-02 23:30:57 +0300

    добавление PUSH node v.16 and <

--- api.js
+++ api.js
@@ -4,13 +4,16 @@ const request = require('request')
 const port = 80
 const bodyParser = require('body-parser');
 const mainRouter = require("./routes/mainRouter.js");
+const fn = require("./controllers/FnController.js");
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/', mainRouter);
+app.get('/push', fn.testPush);
 app.listen(port, () => {
   console.log(`API listening on port ${port}`);
+  fn.massLoop();
 });
 
 // обработка ошибки 404
--- controllers/FnController.js
+++ controllers/FnController.js
@@ -2,6 +2,11 @@ const fetch = require('node-fetch');
 const fs = require('fs').promises;
 var moment = require('moment');
 const nodemailer = require('nodemailer');
+const DB = require("./DbController.js");
+const mysql = require('mysql2/promise');
+const firebase = require("firebase-admin");
+const serviceAccount = require('../docs/crowdfaster-9a11a-firebase-adminsdk-uc1pk-f60b5ea352.json');
+
 
 exports.encrypt = async (plaintext)=>{
     const params = new URLSearchParams();
@@ -66,5 +71,89 @@ exports.sendMail = async (email, subject, text, html ='')=>{
         return false;
     }
 }
+
+exports.massLoop =async ()=>{
+    setInterval(eventMass, 3000);
+    async function eventMass(){
+        try{
+            const conn = await mysql.createConnection(DB.config);
+            let sql = `SELECT 
+            chat_messages_id as messages_id,
+            chat_messages_date as date,
+            chat_messages_id_hash as chat_id_hash,
+            chat_messages_action as type,
+            chat_messages_id_user as user_id
+            FROM uni_chat_messages WHERE chat_messages_send=0 AND chat_messages_status = 0`;
+            let [rows,fields]=await conn.execute(sql);
+            let userMassages = rows;
+            if(userMassages.length>0){
+                for(let msg = 0; msg<userMassages.length; msg++){
+                    let messages_id = userMassages[msg].messages_id;
+                    let user_id = userMassages[msg].user_id;
+                    //проверяем, есть ли у него dev_id
+                    let sql =`SELECT dev_id FROM uni_userdata WHERE user_id=${user_id}`;
+                    let [rows,fields]=await conn.execute(sql);
+                    console.log("Сообщение для user_id", user_id);
+                    if(rows.length>0){
+                        let devs = rows;
+                        for(let dev=0; dev<devs.length; dev++){
+                            let sev_id = devs[dev].dev_id;
+                            if(sev_id){
+                                //
+                                await sendPush(dev_id, type, title ,text);
+                            }
+                            
+                        }
+                    }
+                    sql = `UPDATE uni_chat_messages 
+                    SET chat_messages_send = 1 WHERE chat_messages_id=${messages_id}`;
+                    [rows,fields]=await conn.execute(sql);
+                }
+            }
+            conn.end();
+        }catch(error){
+            console.log(error);
+        }
+    }
+}
+async function sendPush(dev_id, title ,text){
+    //инициализация приложения
+    firebase.initializeApp({
+        credential: firebase.credential.cert(serviceAccount),
+        databaseURL: "https://crowdfaster-9a11a-default-rtdb.europe-west1.firebasedatabase.app"
+      });
+    const payload = {
+        notification: {
+          title: title,
+          body: text,
+        }
+      };
+      const options = {
+        priority: 'high',
+        timeToLive: 60 * 60 * 24, // 1 day
+      };
+      await firebase.messaging().sendToDevice(dev_id, payload, options);
+}
+exports.testPush = async (req, res)=>{
+        let dev_id = "qwri3bl3b543";
+        //инициализация приложения
+        firebase.initializeApp({
+            credential: firebase.credential.cert(serviceAccount),
+            databaseURL: "https://crowdfaster-9a11a-default-rtdb.europe-west1.firebasedatabase.app"
+          });
+    
+        const payload = {
+            notification: {
+              title: "Это тяйтл",
+              body: "Это боди",
+            }
+          };
+          const options = {
+            priority: 'high',
+            timeToLive: 60 * 60 * 24, // 1 day
+          };
+          console.log( await firebase.messaging().sendToDevice(dev_id, payload, options));
+}
+
 
 exports.translit = (str)=>{ var magic = function(lit){ var arrayLits = [ ["а","a"], ["б","b"], ["в","v"], ["г","g"], ["д","d"], ["е","e"], ["ё","yo"], ["ж","zh"], ["з","z"], ["и","i"], ["й","j"], ["к","k"], ["л","l"], ["м","m"], ["н","n"], ["о","o"], ["п","p"], ["р","r"], ["с","s"], ["т","t"], ["у","u"], ["ф","f"], ["х","h"], ["ц","c"], ["ч","ch"], ["ш","w"], ["щ","shh"], ["ъ","''"], ["ы","y"], ["ь","'"], ["э","e"], ["ю","yu"], ["я","ya"], ["А","A"], ["Б","B"], ["В","V"], ["Г","G"], ["Д","D"], ["Е","E"], ["Ё","YO"], ["Ж","ZH"], ["З","Z"], ["И","I"], ["Й","J"], ["К","K"], ["Л","L"], ["М","M"], ["Н","N"], ["О","O"], ["П","P"], ["Р","R"], ["С","S"], ["Т","T"], ["У","U"], ["Ф","F"], ["Х","H"], ["Ц","C"], ["Ч","CH"], ["Ш","W"], ["Щ","SHH"], ["Ъ",""], ["Ы","Y"], ["Ь",""], ["Э","E"], ["Ю","YU"], ["Я","YA"], ["0","0"], ["1","1"], ["2","2"], ["3","3"], ["4","4"], ["5","5"], ["6","6"], ["7","7"], ["8","8"], ["9","9"], ["a", "a"], ["b", "b"], ["c", "c"], ["d", "d"], ["e", "e"], ["f", "f"], ["g", "g"], ["h", "h"], ["i", "i"], ["j", "j"], ["k", "k"], ["l", "l"], ["m", "m"], ["n", "n"], ["o", "o"], ["p", "p"], ["q", "q"], ["r", "r"], ["s", "s"], ["t", "t"], ["u", "u"], ["v", "v"], ["w", "w"], ["x", "x"], ["y", "y"], ["z", "z"], ["A", "A"], ["B", "B"], ["C", "C"], ["D", "D"], ["E", "E"], ["F", "F"], ["G", "G"], ["H", "H"], ["I", "I"], ["J", "J"], ["K", "K"], ["L", "L"], ["M", "M"], ["N", "N"], ["O", "O"], ["P", "P"], ["Q", "Q"], ["R", "R"], ["S", "S"], ["T", "T"], ["U", "U"], ["V", "V"], ["W", "W"], ["X", "X"], ["Y", "Y"], ["Z", "Z"], [" ", "_"] ]; var efim360ru = arrayLits.map(i=>{if (i[0]===lit){return i[1]}else{return undefined}}).filter(i=>i!=undefined); if (efim360ru.length>0){return efim360ru[0]} else{return "-"} }; return Array.from(str).map(i=>magic(i)).join("") }
